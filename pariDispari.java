42d9c278992028f4b187ec4e3f298303dc95f1bd
polgoriunova <polina.goriunova@gmail.com>
2020-02-09 20:44:36 +0100

    Update pariDispari.java

--- pariDispari.java
+++ pariDispari.java
@@ -1,96 +1,112 @@
-public class ArrayPariDispari {
+public class Main {
 
     public static void main(String[] args) {
 
 
 
-        Integer a [] = {9,4,7,2,6,11};     //4, 2, 6, 38, 22, 24
-        Integer b [] = {45,38,22,23,24,5}; // 9, 7,11, 45, 23, 5
+        Integer a [] = {9,4,7,2,6,11,80,43,27,9};
+        Integer b [] = {45,38,22,23,24,5};
 
-        //count pari dispari a
+        System.out.println("Initial arrays:");
+        printArray(a);
+        printArray(b);
+        
+
+        Integer[] pari = createPari(a, b);
+        System.out.println("Unsorted array pari:");
+        printArray(pari);
+
+        Integer[] dispari = createDispari(a,b);
+        System.out.println("Unsorted array dispari:");
+        printArray(dispari);
+
+        System.out.println("Sorted array pari:");
+        bubbleSort(pari);
+        printArray(pari);
+
+        System.out.println("Sorted array dispari:");
+        bubbleSort(dispari);
+        printArray(dispari);
+
+
+
+    }
+
+    public static int countPari(Integer[] arr){
         int p = 0;
+        for(int i=0; i<arr.length;i++) {
+            if(arr[i]%2==0) {
+                p++;
+            }
+        }
+        return p;
+
+    }
+    public static int countDispari(Integer[] arr){
         int d = 0;
-        for(int i=0; i<a.length;i++) {
-            if(a[i]%2==0) {
-                p++;
-            } else {
+        for(int i=0; i<arr.length;i++) {
+            if(arr[i]%2!=0) {
                 d++;
             }
         }
-        //count pari dispari b
-        for(int i=0; i<b.length;i++) {
-            if(b[i]%2==0) {
-                p++;
-            } else {
-                d++;
-            }
-        }
-        //System.out.println(p + " " + d);
-
-        //create pari[], dispari[]
-
-        int [] pari = new int[p];
-        int [] dispari = new int[d];
+        return d;
+    }
+    public static Integer[] createPari(Integer[] x, Integer[] y){
         int n=0;
-        int f=0;
-        //put pari, dispari from a[] and b[] to pari[] and dispari[]
 
-        for(int i = 0; i<a.length;i++) {
-            if(a[i]%2==0) {          // 4%2 ==0
-                pari[n] = a[i];      // pari[6] = 4
+        Integer[] pari = new Integer[countPari(x)+countPari(y)];
+
+        for(int i = 0; i<x.length;i++) {
+            if(x[i]%2==0) {
+                pari[n] = x[i];
                 n++;
-            } else {
-                dispari[f] = a[i];  // dispari[6] = 9, 7
-                f++;
             }
         }
 
-        for(int i = 0; i<b.length;i++) {
-            if(b[i]%2==0) {
-                pari[n] = b[i];
+        for(int i = 0; i<y.length;i++) {
+            if(y[i]%2==0) {
+                pari[n] = y[i];
                 n++;
-            } else {
-                dispari[f] = b[i];
-                f++;
             }
         }
-//        for(int i=0; i<p; i++) {
-//            System.out.println(pari[i]);
-//        }
-//        for(int i=0; i<d; i++) {
-//            System.out.println(dispari[i]);
-//        }
+        return pari;
+    }
+    public static Integer[] createDispari(Integer[] x, Integer[] y){
+        int n=0;
 
-        // sort pari[p] and dispari[d]
+        Integer[] dispari = new Integer[countDispari(x)+countDispari(y)];
 
-
-        for(int i=0; i<pari.length-1; i++){
-            for(int j=0; j<pari.length-i-1; j++){
-               if(pari[j]>pari[j+1]) {
-                   int temp = pari[j];
-                   pari[j] = pari[j + 1];
-                   pari[j+1]=temp;
-               }
+        for(int i = 0; i<x.length;i++) {
+            if(x[i]%2!=0) {
+                dispari[n] = x[i];
+                n++;
             }
         }
-        for(int i=0; i<pari.length; i++){
-            System.out.print(pari[i] + " ");
+
+        for(int i = 0; i<y.length;i++) {
+            if(y[i]%2!=0) {
+                dispari[n] = y[i];
+                n++;
+            }
+        }
+        return dispari;
+    }
+    public static void printArray(Integer [] arr){
+        for(int i=0; i<arr.length; i++ ){
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
-        
-        for(int i=0; i<dispari.length-1; i++){
-            for(int j=0; j<dispari.length-i-1; j++){
-                if(dispari[j]>dispari[j+1]) {
-                    int temp = dispari[j];
-                    dispari[j] = dispari[j + 1];
-                    dispari[j+1]=temp;
+    }
+    public static void bubbleSort(Integer [] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j+1]=temp;
                 }
             }
         }
-        for(int i=0; i<dispari.length; i++){
-            System.out.print(dispari[i] + " ");
-        }
-
 
     }
 }
